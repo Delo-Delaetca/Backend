fec8321cc66d2ab4f9f81315f7bec94576d43beb
Delo-Delaetca <artemc151@gmail.com>
2025-01-17 18:34:50 +1000

    2 ДЗ

--- Tasks/mysite/main/views.py
+++ Tasks/mysite/main/views.py
@@ -4,4 +4,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. Я сделал первую задачку?")
+    if request.method == "GET":
+        return render(request, "index.html", {})
+
+
+def auth(request):
+    if request.method == "GET":
+        return render(request, "auth.html", {})
+
+
+def result(request):
+    if request.method == "POST":
+        data = {'name' : request.POST['fname']}
+        return render(request, "resultat.html", data)
